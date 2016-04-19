eb44f14f3e02728762fe56c6d2a8e3150d95fc7c
Raghuram Pamidimarri <Raghu@Raghurams-MacBook-Pro.local>
2016-04-18 23:11:56 -0700

    Tweet table design

--- data-storage/staging-scripts/Create-Tweets-Statistic.sql
+++ data-storage/staging-scripts/Create-Tweets-Statistic.sql
@@ -1,7 +1,8 @@
 CREATE TABLE TweetStatistic (
 imdbId varchar(64),
-tweetTime varchar(64),
 keyword varchar(200),
-sentimentScore varchar(20),
-CONSTRAINT imdbId_tweetTime_stat PRIMARY KEY(imdbId,tweetTime)
+runningCount bigint,
+runningPositiveSentiment real,
+runningNegativeSentiment real,
+CONSTRAINT imdbId_tweetTime_stat PRIMARY KEY(keyword)
 )
--- data-storage/staging-scripts/Create-Tweets-Table.sql
+++ data-storage/staging-scripts/Create-Tweets-Table.sql
@@ -3,6 +3,6 @@ imdbId varchar(64),
 tweetTime varchar(64),
 keyword varchar(200),
 fullTweet varchar(200),
-CONSTRAINT imdbId_tweetTime PRIMARY KEY(imdbId,tweetTime)
+CONSTRAINT imdbId_tweetTime PRIMARY KEY(keyword,tweetTime)
 )
 
