650f479e9a662549bca89c67c2e14a0d0b193523
root <root@ip-10-157-110-184.ec2.internal>
2016-04-25 04:05:35 +0000

    Changes to the SQLs

---
+++ data-storage/staging-scripts/Alter-Table-ActiveMovie.sql
@@ -0,0 +1,2 @@
+ALTER TABLE ActiveMovie ADD COLUMN searchTitle varchar(200);
+ALTER TABLE ActiveMovie ADD COLUMN searchForTweets varchar(10);
--- data-storage/staging-scripts/Create-Tweets-Statistic.sql
+++ data-storage/staging-scripts/Create-Tweets-Statistic.sql
@@ -1,10 +1,11 @@
 DROP TABLE TweetStatistic;
 CREATE TABLE TweetStatistic (
 tmdbId varchar(100),
-keyword varchar(1000),
+title varchar(1000),
 runningCount bigint,
 runningPositiveSentiment real,
 runningNegativeSentiment real,
 runningNeutralSentiment real,
+runningCompoundSentiment real,
 CONSTRAINT tmdbId_stat PRIMARY KEY(tmdbId)
 )
